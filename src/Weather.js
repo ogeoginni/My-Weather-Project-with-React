b390505c61c91c2b8973a80da198ad63ca6415a9
ogeoginni <92025350+ogeoginni@users.noreply.github.com>
2023-03-16 10:49:34 +0100

    Installed bootstrap and axios dependencies

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  return (
+    <div>
+      <h1>Weather App </h1>
+      <footer>This project was coded by {""} Ogechukwu Oginni </footer>
+    </div>
+  );
+}
